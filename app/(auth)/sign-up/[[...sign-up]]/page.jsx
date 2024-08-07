c88531c2b0ad28ab1ed845e54885c70cda9fd8b5
Yash Shailendra Awachare <107410146+Yashawachare@users.noreply.github.com>
2024-08-07 23:23:04 +0530

    Update page.jsx

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -38,15 +38,6 @@ export default function Page() {
               </svg>
             </a>
   
-            <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to Interview Bits🦑
-            </h1>
-  
-            <p className="mt-4 leading-relaxed text-gray-500">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
-            </p>
-          </div>
   
           <SignUp />
         </div>
